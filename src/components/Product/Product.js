bf8005ab640b811c0dfa6da2d0aabc668447c6de
Paweł <p.grabczak@cognitionhub.com>
2024-09-24 15:05:00 +0200

    przed wyzwaniem

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,40 +1,77 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
-const Product = props => {
+const prepareColorClassName = color => {
+  return styles['color' + color.charAt(0).toUpperCase() + color.slice(1)];
+};
+
+const Product = ({title, basePrice, colors, sizes, name}) => { // stan poczatkowy prodóktów
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+
+  const getPrice = () => { // obliczanie ceny
+    const selectedSize = sizes.find(size => size.name === currentSize);
+    return basePrice + selectedSize.additionalPrice;
+  };;
+
+  const handleSubmit = (event) => { //wyświetlanie koszyka w konsoli
+    event.preventDefault();
+    console.log({
+      title,
+      price: getPrice(),
+      selectedColor: currentColor,
+      selectedSize: currentSize
+    });
+  };
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={'${title} - ${currentColor}'}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`}
+        />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {getPrice()} $</span>
         </header>
-        <form>
+         <form onSubmit={handleSubmit}> {/*wywołanie funkcji co wyświetli nam zawartosć koszyka w konsoli */}
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {sizes.map(size => (
+                <li key={size.name}>
+                  <button
+                  type='button'
+                  className={clsx(currentSize === size.name && styles.active)}
+                  onClick={() => setCurrentSize(size.name)}
+                  >
+                    {size.name}
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {colors.map(color => (
+                <li key={color}>
+                  <button
+                  type="button"
+                  className={clsx(prepareColorClassName(color), currentColor === color && styles.active)}
+                  onClick={() => setCurrentColor(color)}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
-          <Button className={styles.button}>
+          <Button className={styles.button} type='submit'>
             <span className="fa fa-shopping-cart" />
           </Button>
         </form>
